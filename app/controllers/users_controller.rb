0c593b7db0ebdd60f9441617132732d6bca68011
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-21 16:31:56 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,9 @@ class UsersController < ApplicationController
     if @user.save 
       session[:user_id] = @user.id 
       redirect_to :welcome 
+    else 
+      redirect_to :new_user 
+    end 
   end 
 
 
