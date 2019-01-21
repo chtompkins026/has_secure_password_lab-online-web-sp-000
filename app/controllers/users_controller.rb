3bec7638906ced4e24862fafe303627e98534db3
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-21 16:30:42 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,5 @@
 class UsersController < ApplicationController
   
-  def new 
-    @user = User.new 
-  end 
-  
- def create
-    @user = User.find_by(name: params[:user][:password])
-    if @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to :welcome 
-    else 
-      
-      redirect_to :login
-    end 
-  end
 
 
 end
