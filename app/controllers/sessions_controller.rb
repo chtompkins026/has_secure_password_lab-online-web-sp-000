e09a98fd69df34f0f088445ef8288259b55fb6b1
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-21 16:59:03 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,10 @@ class SessionsController < ApplicationController
   end 
   
  def create
-    @user = User.find_by(name: params[:name])
-    if @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to :welcome 
-    else 
-      flash[:error] = "Login is incorrect, please try again"
-      redirect_to :login
-    end 
-  end
+    @user = User.find_by(name: params[:user][:name])
+    return head(:forbidden) unless @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect_to '/'
 
   
 end 
