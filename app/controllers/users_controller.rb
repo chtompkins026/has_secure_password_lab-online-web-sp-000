d94c22e8269a43dce4246e6a41603275d516926b
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-21 16:28:12 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,13 @@ class UsersController < ApplicationController
     @user = User.new 
   end 
   
-  def create 
-    
+ def create
+    @user = User.find_by(username: params[:username])
+    return head(:forbidden) unless @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+  end
+
+
   end 
 
 
