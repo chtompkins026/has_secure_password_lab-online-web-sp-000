de463af835af2d526890ca38c703340735e675bb
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-21 16:58:12 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   
  def create
     @user = User.find_by(name: params[:name])
-    if @user.authenticate(params[:password_digest])
+    if @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to :welcome 
     else 
