09fc1e94963341f9d41fc7d63aa133cb2896ae0a
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-21 16:30:01 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
   
  def create
     @user = User.find_by(name: params[:user][:password])
-    return head(:forbidden) unless @user.authenticate(params[:user][:password])
-    session[:user_id] = @user.id
-    redirect_to :welcome 
+    if @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to :welcome 
     
     else 
       redirect_to :login
