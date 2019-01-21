8df77fd5fc5ffee560c47051ecaaa074f3f4bbc5
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-21 16:40:12 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only [:new, :create]
+  resources :users, only: [:new, :create]
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/welcome' => 'welcome#new'
